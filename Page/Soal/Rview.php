4d2a0e8a7d9b98e97fbf94b4cc7f7fcfff4104d3
Unknown <fadillasukmaal@gmail.com>
2018-05-30 15:24:38 +0700

    ...

--- Page/Soal/Rview.php
+++ Page/Soal/Rview.php
@@ -181,11 +181,12 @@ if(isset($_GET['submit'])){
        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal3" style="font-size: 20px">
           <button class="btn btn-primary btn-xl text-uppercase js-scroll-trigger">Edit</button>
         </a> 
-
         <a class="portfolio-link" data-toggle="modal" href="#portfolioModal4" style="font-size: 20px">
              <button class="btn btn-primary btn-xl text-uppercase js-scroll-trigger">Special Hapus</button>
         </a> 
-     <button class="btn btn-primary btn-xl text-uppercase js-scroll-trigger">Buat</button>
+        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal5" style="font-size: 20px">
+             <button class="btn btn-primary btn-xl text-uppercase js-scroll-trigger">Tambah</button>
+        </a>
   </div>
 </div>
 
@@ -290,7 +291,36 @@ if(isset($_GET['submit'])){
       </div>
     </div>
 
-  
+    <div class="portfolio-modal modal fade" id="portfolioModal5" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 mx-auto">
+                <div class="modal-body">
+                  <h2 class="text-uppercase">Tambah</h2>        
+                  <p class="text-muted">Menambahkan menu baru</p>
+               <div class="mencari">
+                <form method="POST" action="../../Database/query/Soal/FTrigger.php" >
+                    <input id="menuid" class="inp" type="text" name="menuid" placeholder="ID Menu">
+                    <input id="nama" class="inp" type="text" name="nama" placeholder="Nama Menu">
+                    <input id="jenis" class="inp" type="text" name="jenis" placeholder="Jenis Menu">
+                    <input id="harga" class="inp" type="text" name="harga" placeholder="Harga Menu">
+                    <button  id="submit-search" type="submit" name="submit-search" > Add </button>
+                    <div id="responses" class="responses"></div>
+                </form>
+              </div>
+                <button class="btn btn-primary" data-dismiss="modal" type="button">
+                    <i class="fa fa-times"></i>
+                    Close</button> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>  
+
  
     <!-- Bootstrap core JavaScript -->
  
